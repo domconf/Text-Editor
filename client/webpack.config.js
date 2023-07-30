23010fad9bafccb624014ccd4aea984a59dcb470
Dominick Confessore <domconfessore@gmail.com>
2023-07-30 13:25:41 -0400

    Updated template to correct index.html

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -19,7 +19,7 @@ module.exports = () => {
     },
     plugins: [
       new HtmlWebpackPlugin({
-        template: ".index.html",
+        template: "index.html",
         title: "JATE",
       }),
       //injects our custom service work from src-sw.js
